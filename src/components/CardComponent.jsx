dae6274eabcddb57bca7255eb1aab1a487c5df92
Diego Vilhlava <diegorocha450@yahoo.com>
2024-07-02 10:27:43 -0300

    home page updated

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,14 +1,40 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Box, Flex, Image, Text } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 import { imagePath } from "../services/api"
+import { StarIcon } from "@chakra-ui/icons";
 
 
-const CardComponent = ({item}) => {
+const CardComponent = ({ item }) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = new Date(item?.release_date || item?.first_air_date).toLocaleDateString('pt-BR', options);
+
+
+
+
   return (
     <Link to={'/'}>
-        <Box>
-            <Image src={`${imagePath}/${item.poster_path}`}/>
+      <Box position={'relative'} transform={'scale(1)'} _hover={{
+        transform: { base: 'scale(1)', md: 'scale(1.08)' },
+        zIndex: '10',
+        transition: 'transform 0.2s ease-in-out',
+        '& .overlay': {
+          opacity: 1
+        }
+      }}>
+        <Image src={`${imagePath}/${item.poster_path}`} alt={item?.title || item?.name} height={'100%'} />
+        <Box
+          className="overlay"
+          position={'absolute'} p={'2'} bottom={'0'} left={'0'} width={'100%'} h={'33%'} bg="rgba(0,0,0,0.9)" opacity={'0'} transition={'opacity 0.3s ease-in-out'}>
+          <Text textAlign={'center'}>{item?.title || item?.name}</Text>
+          <Text textAlign={'center'} fontSize={'small'} color={'green.200'}>
+            {formattedDate}
+          </Text>
+          <Flex alignItems={"center"} justifyContent={"center"} gap={2} mt="2">
+            <StarIcon fontSize={"small"} />
+            <Text>{item?.vote_average?.toFixed(1)}</Text>
+          </Flex>
         </Box>
+      </Box>
     </Link>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,50 @@
-import { Box, Container, Flex, Grid, Heading, Image } from "@chakra-ui/react"
+import { Box, Container, Flex, Grid, Heading, Skeleton, } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
-import { fetchTrending} from "../services/api"
+import { fetchTrending } from "../services/api"
 import CardComponent from "../components/CardComponent"
 
 
 const Home = () => {
-    const [data,setData] = useState([])
-    const [timeWindow,setTimeWindow] = useState('day')
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [timeWindow, setTimeWindow] = useState('day')
     useEffect(() => {
-        fetchTrending(timeWindow).then((res) => { setData(res)}).catch((err) => console.log(err))
+        fetchTrending(timeWindow).then((res) => {
+            setLoading(true)
+            setData(res)
+
+        }).catch((err) => console.log(err)).finally(() => {
+            setLoading(false)
+        })
     }, [timeWindow])
     return (
         <Container maxW={'container.xl'}>
             <Flex alignItems={'baseline'} gap={'4'} my={'10'}>
 
-            <Heading as={'h2'} fontSize={'md'} textTransform={'uppercase'}>Tendências</Heading>
-            <Flex alignItems={'center'} gap={'2'} border={'1px solid teal'} borderRadius={'20px'} >
-                <Box as='button' px={'3'} py={'1'} borderRadius={'20px'}  
-                onClick={() => setTimeWindow('day')}>Hoje</Box>
-                <Box as='button' px={'3'} py={'1'} borderRadius={'20px'}
-                onClick={() => setTimeWindow('week')}
-                >Nesta Semana</Box>
+                <Heading as={'h2'} fontSize={'md'} textTransform={'uppercase'}>Tendências</Heading>
+                <Flex alignItems={'center'} gap={'2'} border={'1px solid teal'} borderRadius={'20px'} >
+                    <Box as='button' px={'3'} py={'1'} borderRadius={'20px'}
+                        onClick={() => setTimeWindow('day')} bg={`${timeWindow === 'day' ? 'gray.800' : ''}`}>Hoje</Box>
+                    <Box as='button' px={'3'} py={'1'} borderRadius={'20px'}
+                        onClick={() => setTimeWindow('week')}
+                        bg={`${timeWindow === 'week' ? 'gray.800' : ''}`}
+                    >Nesta Semana</Box>
+                </Flex>
             </Flex>
-            </Flex>
-            <Grid templateColumns={{base:"1fr",sm:"repeat(2,1fr)",md:"repeat(4,1fr)",lg:"repeat(5,1fr)" }} gap={'4'}>
-                {data && data?.map((item) => (
-                    <CardComponent key={item.id} item={item} />
-                ))}
+          
+            <Grid templateColumns={{ base: "1fr", sm: "repeat(2,1fr)", md: "repeat(4,1fr)", lg: "repeat(5,1fr)" }} gap={'4'}>
+            {data &&
+          data?.map((item, i) =>
+            loading ? (
+              <Skeleton height={300} key={i} />
+            ) : (
+              <CardComponent
+                key={item?.id}
+                item={item}
+                type={item?.media_type}
+              />
+            )
+          )}
             </Grid>
         </Container>
     )
